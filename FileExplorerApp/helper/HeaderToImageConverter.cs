1cf89e62301d8091f75cff2d035f682f3e40c628
Fulop Zoltan <fulopzoltan20@gmail.com>
2022-02-21 13:49:19 +0200

    FileExplorerApp finished

---
+++ FileExplorerApp/helper/HeaderToImageConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FileExplorerApp.helper
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter headerToImageConverter = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string) value;
+            if (path == null)
+                return null;
+            string name = MainWindow.getFileFolderName(path);
+            string image = "Images/file.png";
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder.png";
+            return new BitmapImage(new Uri($"pack://application:,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
