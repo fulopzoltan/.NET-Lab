1cb3738cbbad352f516d66ff2074951774dfc9db
Fulop Zoltan <fulopzoltan20@gmail.com>
2022-02-28 10:44:25 +0200

    Product app finish

---
+++ ProductApp/ProductApp/Helper/IntegerToBrushConverterSales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ProductApp.Helper;
+
+public class IntegerToBrushConverterSales : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (int.Parse(value.ToString()) < 100) ? Brushes.Gray : Brushes.LawnGreen;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
